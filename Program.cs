6739356c5614ce0ff7a31e31fdc190023527c416
Top <Top@8-16>
2025-02-12 15:09:44 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string UserInput;
+            int tmpNum = 0;
+            while (true)
+            { 
+                Console.Write("Введите число: ");
+                UserInput = Console.ReadLine();
+                if (UserInput == "")
+                {
+                    break;
+                }
+                int parseUInput = int.Parse(UserInput);
+                if (tmpNum < parseUInput)
+                {
+                    tmpNum = parseUInput;
+                }
+            }
+        }
+    }
+}
+
